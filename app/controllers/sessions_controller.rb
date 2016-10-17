dc2d5c753571cc1c0eaeb5477578cc1b6ae4472b
TomMulvaney <mulvaney.tom@gmail.com>
2016-10-17 18:55:08 +0100

    Added integration test for logging out. Deleted comments in auth_test and sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,17 +20,12 @@ class SessionsController < ApplicationController
   end
 
   def login_attempt
-    puts "Logging in"
-    puts params[:username_or_email]
-    puts params[:password]
     authorized_user = User.authenticate(params[:username_or_email],params[:password])
     if authorized_user
-      puts "Authorized"
       session[:user_id] = authorized_user.id
       flash[:notice] = "Wow Welcome again, you logged in as #{authorized_user.username}"
       redirect_to(action: 'home')
     else
-      puts "Unauthorized"
       flash[:notice] = "Invalid Username or Password"
       flash[:color]= "invalid"
       render "login"
--- spec/integration/auth_test.rb
+++ spec/integration/auth_test.rb
@@ -10,12 +10,16 @@ def login (user)
   end
 end
 
+def get_password
+  FactoryGirl.build(:user).password
+end
+
 RSpec.describe "authentication", type: :feature do
   let(:user) { FactoryGirl.create(:user) }
   subject { user }
 
   it "logs in successfully" do
-    user.password = FactoryGirl.build(:user).password
+    user.password = get_password()
     login(user)
     expect(current_path).to eq(home_path)
   end
@@ -23,5 +27,13 @@ RSpec.describe "authentication", type: :feature do
   it "logs in with incorrect password" do
     user.password = "wrong password"
     login(user)
+    expect(current_path).to eq(sessions_login_attempt_path)
+  end
+
+  it "logs out" do
+    user.password = get_password()
+    login(user)
+    click_on("logout")
+    expect(current_path).to eq(root_path)
   end
 end
