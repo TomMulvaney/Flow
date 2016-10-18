ebe8ead61da73662fb67559f7c6171060de0f55a
TomMulvaney <mulvaney.tom@gmail.com>
2016-10-18 20:31:13 +0100

    Created intensities factory and model test

---
+++ spec/factories/intensities.rb
@@ -0,0 +1,8 @@
+require 'factory_girl'
+
+FactoryGirl.define do
+  factory :intensity do
+    name "Test Intensity"
+    level 9001
+  end
+end
---
+++ spec/models/intensity_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+require 'factory_girl'
+require 'bcrypt'
+require 'shoulda-matchers'
+
+RSpec.describe Intensity, type: :model do
+  let(:intensity) { FactoryGirl.build(:intensity) }
+  subject { intensity }
+
+  it { should be_valid }
+  it { should validate_presence_of(:level) }
+  it { should validate_uniqueness_of(:level) }
+  it { should have_many(:track_intensities) }
+  it { should have_many(:tracks).through(:track_intensities) }
+end
